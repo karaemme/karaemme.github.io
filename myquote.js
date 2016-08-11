35911999565b2d828ef5dab2ea17cd1307107f89
karaemme <kara.emma.k@gmail.com>
2016-08-10 22:16:46 -0400

    Update myquote.js

--- myquote.js
+++ myquote.js
@@ -10,7 +10,7 @@ var Great = ["Outstanding", "Perfect", "Important"];
 var write = ["compose", "compile", "note", "communicate", "record", "produce", "formulate"];
 var humans = ["beings", "individuals", "humanoids", "mortals", "earthlings"];
 
-var programmer = ["developer", "drafter", "hobbyist", "coder", "try hard engineer"];
+var programmer = ["developer", "computer whisperer", "hobbyist", "coder", "try hard engineer"];
 
 function choosei(){
     var i = Math.floor(Math.random() * 4);
@@ -29,7 +29,7 @@ var q = document.getElementById("quote");
 
 q.innerHTML =
     "\"Any " + fools +
-    "can write code that a computer can " +
-    understands + "." + Greats + " programmers " + writes + " code that " + human + " can " + understands + ".\"" +
-    +  "Martin Fowler, " + programmers;
+    " can write code that a computer can " +
+    understands + ". " + Greats + " programmers " + writes + " code that " + human + " can " + understands + ".\"" +
+      "Martin Fowler, " + programmers;
 
